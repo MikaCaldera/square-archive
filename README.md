db4b2f4038c5973ec52fa026ac6215bc42db4d72
Mika Caldera <mika.caldera@gmail.com>
2022-06-08 11:45:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# :briefcase: 2022 Web Portfolio - Squarespace Untouched  
+
+**Status of Site:** _archived_  
+**Launch date:**  _not for launch_  
+**CMS:** _n/a_  
+**Theme:** _ss_  
+**Foundation:** _Bootstrap_  
+**Server:** none  
+  
+
+## :busts_in_silhouette: Team  
+
+ - Designer: Mika Caldera  
+
+### :bulb: Powered By
+##### Ownership Mika Caldera | Copyright © 2022
---
+++ assets.squarespace.com/universal/scripts-compressed/extract-css-runtime-f5a5b8af3c7181000db9-min.en-US.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// placeholder script for webpack runtime chunk
+// required for updating rollup arch
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXh0cmFjdC1jc3MtcnVudGltZS03ZGU5MDIyZjEyYjk0OTJlZmVkNC1taW4uZW4tVVMuanMiLCJtYXBwaW5ncyI6Ijs7O0FBQUE7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL2V4dHJhY3QtY3NzLy4vc3JjL2FwcHMvRXh0cmFjdENTU1J1bnRpbWUvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gcGxhY2Vob2xkZXIgc2NyaXB0IGZvciB3ZWJwYWNrIHJ1bnRpbWUgY2h1bmtcbi8vIHJlcXVpcmVkIGZvciB1cGRhdGluZyByb2xsdXAgYXJjaFxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
